c6092addefdfc81ca261a14389ce7947a9b5a156
Razvan Codreanu <razvan.codreanu90@gmail.com>
2025-08-21 19:16:01 +0300

    Add CNAME for custom domain

---
+++ pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main>
+            <h1>Niponn General Company</h1>
+            <p>Confecții metalice și construcții civile și industriale</p>
+        </main>
+    );
+}
---
+++ pages/pages/pages/despre.js
@@ -0,0 +1,8 @@
+export default function Despre() {
+    return (
+        <main>
+            <h1>Despre noi</h1>
+            <p>Suntem specializați în structuri metalice și construcții industriale.</p>
+        </main>
+    );
+}
